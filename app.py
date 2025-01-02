631bea996764290ca256a0c5435af143f8153a2c
Shrut_Jain <148540392+Shrut2112@users.noreply.github.com>
2025-01-03 02:30:11 +0530

    formating done

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask,render_template,request
 import numpy as np
 import pickle
+
 popular_df=pickle.load(open('popular.pkl','rb'))
 final_table=pickle.load(open('final_table.pkl','rb'))
 similarity_score=pickle.load(open('similarity_score.pkl','rb'))
 books=pickle.load(open('books.pkl','rb'))
+
 app=Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('index.html',book_name=list(popular_df['Book-Title'].values),image=list(popular_df['Image-URL-M']),author=list(popular_df['Book-Author']),votes=list(popular_df['num-rating']),score=list(popular_df['avg-rating']))
+
 @app.route('/recommend')
 def recommend():
     return render_template('recommend.html')
+
 @app.route('/recommend_books',methods=['post'])
 def recommendation():
     user_input = request.form.get('user_input')
@@ -26,7 +31,6 @@ def recommendation():
         item.extend(list(temp_data.drop_duplicates('Book-Title')['Image-URL-M'].values))
         data.append(item)
     return render_template('recommend.html',data=data)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
